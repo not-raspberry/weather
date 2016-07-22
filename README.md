9fe56291c7e71d7facbbc61184c0404bd1bb8b51
Michał Pawłowski <unittestablecode@gmail.com>
2016-07-22 16:58:19 +0200

    Initialize from the figwheel template and remove/adjust some stuff

---
+++ README.md
@@ -0,0 +1,9 @@
+# weather
+
+A weather web app.
+
+## License
+
+Distributed under the Eclipse Public License. See the LICENSE file.
+
+Copyright © 2016 not-raspberry (https://github.com/not-raspberry/).
---
+++ src/clj/weather/handler.clj
@@ -0,0 +1,34 @@
+(ns weather.handler
+  (:require [compojure.core :refer [GET defroutes]]
+            [compojure.route :refer [not-found resources]]
+            [hiccup.page :refer [include-js include-css html5]]
+            [weather.middleware :refer [wrap-middleware]]
+            [config.core :refer [env]]))
+
+(def mount-target
+  [:div#app
+      [:h3 "It's loading..."]])
+
+(defn head []
+  [:head
+   [:meta {:charset "utf-8"}]
+   [:meta {:name "viewport"
+           :content "width=device-width, initial-scale=1"}]
+   (include-css (if (env :dev) "/css/site.css" "/css/site.min.css"))])
+
+(def loading-page
+  (html5
+    (head)
+    [:body {:class "body-container"}
+     mount-target
+     (include-js "/js/app.js")]))
+
+
+(defroutes routes
+  (GET "/" [] loading-page)
+  (GET "/about" [] loading-page)
+
+  (resources "/")
+  (not-found "Not Found"))
+
+(def app (wrap-middleware #'routes))
