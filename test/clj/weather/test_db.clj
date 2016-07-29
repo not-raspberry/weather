cad1de41400f3c81113a324ed5a3ab616da29bc1
Michał Pawłowski <unittestablecode@gmail.com>
2016-07-29 09:25:51 +0200

    Tests for conditions inserting and querying

--- test/clj/weather/test_db.clj
+++ test/clj/weather/test_db.clj
@@ -57,6 +57,61 @@
       (dissoc :date)))
 
 
+(deftest test-conditions-data-upsert
+  (let [date (t/local-date 2016 7 27)
+
+        day-conditions-no-description
+        {:hi 22 :low 15 :description "" :date date}
+
+        day-conditions-warmer-with-description
+        (assoc day-conditions-no-description
+               :hi 24 :low 18 :description "Quite nice")
+
+        day-conditions-warmer-no-description
+        (assoc day-conditions-warmer-with-description :description "")
+
+        fetch-forecast
+        (fn [] (db/weather-forecast
+                 db/*db* {:date-today (to-sql-date date)}))]
+
+    (testing "Conditions are saved to the DB"
+      (save-conditions [day-conditions-no-description])
+      (is (= (fetch-forecast)
+             [(parsed-api-response->expected-db-response
+                day-conditions-no-description date)])))
+
+    (testing "Can update the row."
+      (save-conditions [day-conditions-warmer-with-description])
+      (is (= (fetch-forecast)
+             [(parsed-api-response->expected-db-response
+                day-conditions-warmer-with-description date)])))
+
+    (testing "Updating the row with empty description leaves the original description."
+      (save-conditions [day-conditions-warmer-no-description])
+      (is (= (fetch-forecast)
+             [(parsed-api-response->expected-db-response
+                day-conditions-warmer-with-description date)])))))
+
+(deftest test-conditions-data-query-time
+  (let [date (t/local-date 2014 1 3)
+        query-date-2-days-before (t/minus date (t/days 2))
+        query-date-too-late (t/plus date (t/days 1))
+        day-conditions {:hi 22 :low 15 :description "Sth." :date date}]
+
+    (save-conditions [day-conditions])
+
+    (testing "Future conditions should be reported"
+      (doseq [query-date [date query-date-2-days-before]]
+        (is (= (db/weather-forecast db/*db* {:date-today
+                                             (to-sql-date query-date)})
+               [(parsed-api-response->expected-db-response
+                  day-conditions query-date)]))))
+
+    (testing "Past conditions should be ignored"
+      (is (= (db/weather-forecast db/*db* {:date-today
+                                           (to-sql-date query-date-too-late)})
+             [])))))
+
 (deftest test-fetch-save-query-round-trip
   (doseq [[fetching-fn expected-historical-rows-count
            expected-historical-temperatures]
